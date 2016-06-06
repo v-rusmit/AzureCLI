2c477c874d6c107bae8580edd148c01edcb00906
Kevin Bergman <KBergman@valorem.com>
2016-06-06 12:05:56 -0500

    baseline copy from nTier to vdc

---
+++ xxx-VirtualDatacenter/Scripts/deploy.ps1
@@ -0,0 +1,14 @@
+﻿Login-AzureRmAccount
+Get-AzureRmSubscription -SubscriptionId "36da8c44-d1a8-4e47-9515-7ce3097645fc"
+Get-AzureRmSubscription -SubscriptionId "36da8c44-d1a8-4e47-9515-7ce3097645fc" | Select-AzureRmSubscription
+
+#Get-AzureRmResourceProvider -ListAvailable
+
+$filespec="C:\Users\KBergman\Source\Repos\AzureCLI\4-VirtualDatacenter\Templates"
+$rg  = "play4"
+$loc = "CentralUS"
+
+#New-AzureRmResourceGroup  -Location $loc -Name $rg
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile "$filespec\azuredeploy.json" -TemplateParameterFile "$filespec\azuredeploy.parameters.json" -DeploymentDebugLogLevel All
+                                                  
