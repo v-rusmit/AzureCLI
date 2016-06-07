338d8fa9a8d55c2d58bea8b436405622ad2a0132
Kevin Bergman <KBergman@valorem.com>
2016-06-07 14:41:23 -0500

    base sql complete, moved vnet definitions around

--- 4-VirtualDatacenter/Scripts/deploy.ps1
+++ 4-VirtualDatacenter/Scripts/deploy.ps1
@@ -5,10 +5,10 @@ Get-AzureRmSubscription -SubscriptionId "36da8c44-d1a8-4e47-9515-7ce3097645fc" |
 #Get-AzureRmResourceProvider -ListAvailable
 
 $filespec="C:\Users\KBergman\Source\Repos\AzureCLI\4-VirtualDatacenter\Templates"
-$rg  = "play4"
+$rg  = "play6"
 $loc = "CentralUS"
 
 New-AzureRmResourceGroup -Location $loc -Name $rg
 
 New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile "$filespec\azuredeploy.json" -TemplateParameterFile "$filespec\azuredeploy.parameters.json"    
-                                                  
+                  
