6b5715445aa394eef18cb3ab2e963f6e7ac6ad6a
Kevin Bergman <KBergman@valorem.com>
2016-06-08 08:23:09 -0500

    baseline VDC working

--- 4-VirtualDatacenter/Scripts/deploy.ps1
+++ 4-VirtualDatacenter/Scripts/deploy.ps1
@@ -5,7 +5,7 @@ Get-AzureRmSubscription -SubscriptionId "36da8c44-d1a8-4e47-9515-7ce3097645fc" |
 #Get-AzureRmResourceProvider -ListAvailable
 
 $filespec="C:\Users\KBergman\Source\Repos\AzureCLI\4-VirtualDatacenter\Templates"
-$rg  = "play6"
+$rg  = "play4"
 $loc = "CentralUS"
 
 New-AzureRmResourceGroup -Location $loc -Name $rg
