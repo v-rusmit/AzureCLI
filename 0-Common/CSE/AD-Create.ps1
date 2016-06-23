0f33f81d813ec7eaf4a0c6c62e30172660ccb4f3
Kevin Bergman <KBergman@valorem.com>
2016-06-23 15:01:57 -0500

    configure AD - not complete

---
+++ 0-Common/CSE/AD-Create.ps1
@@ -0,0 +1,10 @@
+﻿param ( $domain, $password )
+
+# for this demo, we will use the classic Contoso for domain and
+#                            ... demoUser and P@ssw0rd1234 for username and password
+
+
+$smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
+
+Install-WindowsFeature -Name "AD-Domain-Services" -IncludeSubFeature
+Install-ADDSForest -DomainName $domain -DomainMode Win2012 -ForestMode Win2012 -Force -SafeModeAdministratorPassword $smPassword
---
+++ 0-Common/CSE/AD-Join.ps1
@@ -0,0 +1,12 @@
+﻿param ( $domain, $password )
+
+# for this demo, we will use the classic Contoso for domain and
+#                            ... demoUser and P@ssw0rd1234 for username and password
+
+$smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
+$username   = "$domain\demoUser" 
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+Install-WindowsFeature -Name "AD-Domain-Services" -IncludeSubFeature
+Add-Computer -DomainName $domain -Credential $credential
+
