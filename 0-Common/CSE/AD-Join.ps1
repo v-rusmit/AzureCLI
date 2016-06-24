2ace2f029eebc3b168a0cdb1078e8e64e959a9bf
Kevin Bergman <KBergman@valorem.com>
2016-06-24 18:31:30 -0500

    refactor password handling

--- 0-Common/CSE/AD-Join.ps1
+++ 0-Common/CSE/AD-Join.ps1
@@ -7,6 +7,6 @@ $smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
 $username   = "$domain\$username" 
 $credential = New-Object System.Management.Automation.PSCredential($username, $smpassword)
 
-Install-WindowsFeature -Name "AD-Domain-Services" -IncludeAllSubFeature
-Add-Computer -DomainName $domain -Credential $credential
+#Install-WindowsFeature -Name "AD-Domain-Services" -IncludeAllSubFeature
+Add-Computer -DomainName $domain -Credential $credential 
 
